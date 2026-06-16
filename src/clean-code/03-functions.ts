2a43ea2d29f9ad3d6cd724d2c8879533db08e33d
mari291 <202300252i@utea.edu.pe>
2026-06-15 22:08:36 -0500

    Forzando subida de archivos 3 y 4

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,21 +1,28 @@
 (() => {
 
-    // 1. Obtener información de una película por ID
+    // función para obtener información de una película por Id
+    function getAllMovies( movieId: string ) {
+        console.log({ movieId });
     function getMovieById(id: string) {
         console.log({ id });
     }
 
-    // 2. Obtener información de los actores de una película por ID
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getAllMovieActors( id: string ) {
     function getMovieCastById(id: string) {
         console.log({ id });
     }
 
-    // 3. Obtener la biografía del actor por ID
+    // funcion para obtener el bio del actor por el id
+    function getUsuario( ActorId: string ) {
+        console.log({ ActorId });
     function getActorBioById(id: string) {
         console.log({ id });
     }
+    
 
-    // 4. Crear una película utilizando una interfaz para los argumentos
+    // Crear una película
+    function movie(title: string, description: string, rating: number, cast: string[] ) {
     interface Movie {
         title: string;
         description: string;
@@ -27,22 +34,33 @@
         console.log({ title, description, rating, cast });
     }
 
-    // 5. Crear un nuevo actor si no existe
-    function createActor(fullName: string, birthdate: Date): boolean {
+    // Crea un nuevo actor
+    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
         
-        // Tarea asíncrona para verificar nombre
+    function createActor(fullName: string, birthdate: Date): boolean {
+
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
         if (fullName === 'fernando') return false;
 
         console.log('Crear actor');
+        return true;        
         return true;
+
     }
 
-    // 6. Calcular el monto a pagar (Uso de retornos tempranos / Clean Code)
     const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
         if (isDead) return 1500;
         if (isSeparated) return 2500;
-        
         return isRetired ? 3000 : 4000;
     }
 
+    
+
+}
+
+
 })();
+)();
