5dd6033da23185f5888aeb6cd53a6915e9fd38a9
mari291 <202300252i@utea.edu.pe>
2026-06-15 19:33:43 -0500

    Ahora sí: solución completa del ejercicio 3

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,4 +1,5 @@
 (() => {
+
     // 1. Obtener información de una película por ID
     function getMovieById(id: string) {
         console.log({ id });
@@ -14,7 +15,7 @@
         console.log({ id });
     }
 
-    // 4. Crear una película utilizando una interfaz
+    // 4. Crear una película utilizando una interfaz para los argumentos
     interface Movie {
         title: string;
         description: string;
@@ -26,20 +27,22 @@
         console.log({ title, description, rating, cast });
     }
 
-    // 5. Crear un nuevo actor
+    // 5. Crear un nuevo actor si no existe
     function createActor(fullName: string, birthdate: Date): boolean {
-        // Tarea asíncrona para verificar nombre ficticia
+        
+        // Tarea asíncrona para verificar nombre
         if (fullName === 'fernando') return false;
 
         console.log('Crear actor');
         return true;
     }
 
-    // 6. Calcular el monto a pagar (Uso de retornos tempranos)
+    // 6. Calcular el monto a pagar (Uso de retornos tempranos / Clean Code)
     const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
         if (isDead) return 1500;
         if (isSeparated) return 2500;
         
         return isRetired ? 3000 : 4000;
     }
+
 })();
